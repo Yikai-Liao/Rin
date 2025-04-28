7eae4fcf096f275a8a046a3b3a8ac704636aebb7
lyk <lyk-boya@outlook.com>
2025-04-29 02:16:34 +0800

    feat: Add "remove from drafts" functionality and update translations in multiple languages

--- client/src/components/feed_card.tsx
+++ client/src/components/feed_card.tsx
@@ -3,6 +3,9 @@ import {useTranslation} from "react-i18next";
 import {timeago} from "../utils/timeago";
 import {HashTag} from "./hashtag";
 import {useMemo} from "react";
+import {useConfirm, useAlert} from "../components/dialog";
+import {client} from "../main";
+import {headersWithAuth} from "../utils/auth";
 
 export function FeedCard({ id, title, avatar, draft, listed, top, summary, hashtags, createdAt, updatedAt }:
     {
@@ -12,47 +15,90 @@ export function FeedCard({ id, title, avatar, draft, listed, top, summary, hasht
         hashtags: { id: number, name: string }[],
         createdAt: Date, updatedAt: Date
     }) {
-    const { t } = useTranslation()
+    const { t } = useTranslation();
+    const { showConfirm, ConfirmUI } = useConfirm();
+    const { showAlert, AlertUI } = useAlert();
+
+    const handleRemoveFromDraft = (e: React.MouseEvent) => {
+        e.preventDefault(); // 防止触发Link的点击事件
+        e.stopPropagation();
+        
+        showConfirm(
+            t("remove_from_draft"),
+            t("remove_from_draft.confirm"),
+            () => {
+                client.feed({ id: parseInt(id) }).post(
+                    {
+                        draft: false,
+                        listed: true
+                    },
+                    {
+                        headers: headersWithAuth(),
+                    }
+                ).then(({ error }) => {
+                    if (error) {
+                        showAlert(error.value as string);
+                    } else {
+                        showAlert(t("remove_from_draft.success"), () => {
+                            window.location.reload();
+                        });
+                    }
+                });
+            }
+        );
+    };
+
     return useMemo(() => (
         <>
-            <Link href={`/feed/${id}`} target="_blank" className="w-full rounded-2xl bg-w my-2 p-6 duration-300 bg-button">
-                {avatar &&
-                    <div className="flex flex-row items-center mb-2 rounded-xl overflow-clip">
-                        <img src={avatar} alt=""
-                            className="object-cover object-center w-full max-h-96 hover:scale-105 translation duration-300" />
-                    </div>}
-                <h1 className="text-xl font-bold text-gray-700 dark:text-white text-pretty overflow-hidden">
-                    {title}
-                </h1>
-                <p className="space-x-2">
-                    <span className="text-gray-400 text-sm" title={new Date(createdAt).toLocaleString()}>
-                        {createdAt === updatedAt ? timeago(createdAt) : t('feed_card.published$time', { time: timeago(createdAt) })}
-                    </span>
-                    {createdAt !== updatedAt &&
-                        <span className="text-gray-400 text-sm" title={new Date(updatedAt).toLocaleString()}>
-                            {t('feed_card.updated$time', { time: timeago(updatedAt) })}
+            <div className="relative w-full">
+                <Link href={`/feed/${id}`} target="_blank" className="w-full rounded-2xl bg-w my-2 p-6 duration-300 bg-button block">
+                    {avatar &&
+                        <div className="flex flex-row items-center mb-2 rounded-xl overflow-clip">
+                            <img src={avatar} alt=""
+                                className="object-cover object-center w-full max-h-96 hover:scale-105 translation duration-300" />
+                        </div>}
+                    <h1 className="text-xl font-bold text-gray-700 dark:text-white text-pretty overflow-hidden">
+                        {title}
+                    </h1>
+                    <p className="space-x-2">
+                        <span className="text-gray-400 text-sm" title={new Date(createdAt).toLocaleString()}>
+                            {createdAt === updatedAt ? timeago(createdAt) : t('feed_card.published$time', { time: timeago(createdAt) })}
                         </span>
+                        {createdAt !== updatedAt &&
+                            <span className="text-gray-400 text-sm" title={new Date(updatedAt).toLocaleString()}>
+                                {t('feed_card.updated$time', { time: timeago(updatedAt) })}
+                            </span>
+                        }
+                    </p>
+                    <p className="space-x-2">
+                        {draft === 1 && <span className="text-gray-400 text-sm">{t("draft")}</span>}
+                        {listed === 0 && <span className="text-gray-400 text-sm">{t("unlisted")}</span>}
+                        {top === 1 && <span className="text-theme text-sm">
+                            {t('article.top.title')}
+                        </span>}
+                    </p>
+                    <p className="text-pretty overflow-hidden dark:text-neutral-500">
+                        {summary}
+                    </p>
+                    {hashtags.length > 0 &&
+                        <div className="mt-2 flex flex-row flex-wrap justify-start gap-x-2">
+                            {hashtags.map(({ name }, index) => (
+                                <HashTag key={index} name={name} />
+                            ))}
+                        </div>
                     }
-                </p>
-                <p className="space-x-2">
-                    {draft === 1 && <span className="text-gray-400 text-sm">{t("draft")}</span>}
-                    {listed === 0 && <span className="text-gray-400 text-sm">{t("unlisted")}</span>}
-                    {top === 1 && <span className="text-theme text-sm">
-                        {t('article.top.title')}
-                    </span>}
-                </p>
-                <p className="text-pretty overflow-hidden dark:text-neutral-500">
-                    {summary}
-                </p>
-                {hashtags.length > 0 &&
-                    <div className="mt-2 flex flex-row flex-wrap justify-start gap-x-2">
-                        {hashtags.map(({ name }, index) => (
-                            <HashTag key={index} name={name} />
-                        ))}
-                    </div>
-                }
-
-            </Link>
+                </Link>
+                {draft === 1 && (
+                    <button
+                        onClick={handleRemoveFromDraft}
+                        className="absolute top-4 right-4 px-2 py-1 bg-theme text-white text-xs rounded-md hover:bg-theme-hover"
+                    >
+                        {t("remove_from_draft")}
+                    </button>
+                )}
+            </div>
+            <ConfirmUI />
+            <AlertUI />
         </>
     ), [id, title, avatar, draft, listed, top, summary, hashtags, createdAt, updatedAt])
 }
