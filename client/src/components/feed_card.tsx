02b6e513511c7623a83fd379b31e767717c0972d
lyk <lyk-boya@outlook.com>
2025-04-29 02:32:46 +0800

    refactor: Improve FeedCard layout by restructuring JSX and enhancing readability

--- client/src/components/feed_card.tsx
+++ client/src/components/feed_card.tsx
@@ -49,13 +49,8 @@ export function FeedCard({ id, title, avatar, draft, listed, top, summary, hasht
     return useMemo(() => (
         <>
             <div className="relative w-full">
-                <Link href={`/feed/${id}`} target="_blank" className="w-full rounded-2xl bg-w my-2 p-6 duration-300 bg-button block">
-                    {avatar &&
-                        <div className="flex flex-row items-center mb-2 rounded-xl overflow-clip">
-                            <img src={avatar} alt=""
-                                className="object-cover object-center w-full max-h-96 hover:scale-105 translation duration-300" />
-                        </div>}
-                    <div className="flex items-center justify-between">
+                <div className="w-full rounded-2xl bg-w my-2 p-6 duration-300 bg-button">
+                    <div className="flex items-center justify-between mb-2">
                         <h1 className="text-xl font-bold text-gray-700 dark:text-white text-pretty overflow-hidden">
                             {title}
                         </h1>
@@ -69,34 +64,43 @@ export function FeedCard({ id, title, avatar, draft, listed, top, summary, hasht
                             </button>
                         )}
                     </div>
-                    <p className="space-x-2">
-                        <span className="text-gray-400 text-sm" title={new Date(createdAt).toLocaleString()}>
-                            {createdAt === updatedAt ? timeago(createdAt) : t('feed_card.published$time', { time: timeago(createdAt) })}
-                        </span>
-                        {createdAt !== updatedAt &&
-                            <span className="text-gray-400 text-sm" title={new Date(updatedAt).toLocaleString()}>
-                                {t('feed_card.updated$time', { time: timeago(updatedAt) })}
+                    
+                    <Link href={`/feed/${id}`} target="_blank" className="block">
+                        {avatar &&
+                            <div className="flex flex-row items-center mb-2 rounded-xl overflow-clip">
+                                <img src={avatar} alt=""
+                                    className="object-cover object-center w-full max-h-96 hover:scale-105 translation duration-300" />
+                            </div>}
+                        
+                        <p className="space-x-2">
+                            <span className="text-gray-400 text-sm" title={new Date(createdAt).toLocaleString()}>
+                                {createdAt === updatedAt ? timeago(createdAt) : t('feed_card.published$time', { time: timeago(createdAt) })}
                             </span>
+                            {createdAt !== updatedAt &&
+                                <span className="text-gray-400 text-sm" title={new Date(updatedAt).toLocaleString()}>
+                                    {t('feed_card.updated$time', { time: timeago(updatedAt) })}
+                                </span>
+                            }
+                        </p>
+                        <p className="space-x-2">
+                            {draft === 1 && <span className="text-gray-400 text-sm">{t("draft")}</span>}
+                            {listed === 0 && <span className="text-gray-400 text-sm">{t("unlisted")}</span>}
+                            {top === 1 && <span className="text-theme text-sm">
+                                {t('article.top.title')}
+                            </span>}
+                        </p>
+                        <p className="text-pretty overflow-hidden dark:text-neutral-500">
+                            {summary}
+                        </p>
+                        {hashtags.length > 0 &&
+                            <div className="mt-2 flex flex-row flex-wrap justify-start gap-x-2">
+                                {hashtags.map(({ name }, index) => (
+                                    <HashTag key={index} name={name} />
+                                ))}
+                            </div>
                         }
-                    </p>
-                    <p className="space-x-2">
-                        {draft === 1 && <span className="text-gray-400 text-sm">{t("draft")}</span>}
-                        {listed === 0 && <span className="text-gray-400 text-sm">{t("unlisted")}</span>}
-                        {top === 1 && <span className="text-theme text-sm">
-                            {t('article.top.title')}
-                        </span>}
-                    </p>
-                    <p className="text-pretty overflow-hidden dark:text-neutral-500">
-                        {summary}
-                    </p>
-                    {hashtags.length > 0 &&
-                        <div className="mt-2 flex flex-row flex-wrap justify-start gap-x-2">
-                            {hashtags.map(({ name }, index) => (
-                                <HashTag key={index} name={name} />
-                            ))}
-                        </div>
-                    }
-                </Link>
+                    </Link>
+                </div>
             </div>
             <ConfirmUI />
             <AlertUI />
